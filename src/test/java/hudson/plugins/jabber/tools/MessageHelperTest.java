e5b4e4b6889d5c4ee53f7c16525ecb090136cf1a
vsellier <vsellier@dev.java.net>
2008-12-11 23:10:38 +0000

    [jabber-plugin] reflexion about more tests

--- src/test/java/hudson/plugins/jabber/tools/MessageHelperTest.java
+++ src/test/java/hudson/plugins/jabber/tools/MessageHelperTest.java
@@ -12,11 +12,18 @@ public class MessageHelperTest {
 		assertEquals(1, MessageHelper.extractCommandLine("test  ").length);
 		assertEquals(3,
 				MessageHelper.extractCommandLine("test param1 param2").length);
-		// TODO desactivate to not break the global build
+		// TODO commented to not break the global build
 		// assertEquals(2,
 		// MessageHelper.extractCommandLine("test \"same param\"").length);
-		// assertEquals(2,
+		// ' is not a separator
+		// assertEquals(3,
 		// MessageHelper.extractCommandLine("test 'same param'").length);
+		
+		// TODO add tests for :
+		// "param1 param's param3" => 3
+		// 'param1 "same param"' => 2
+		// 'param1 "same param" param3' => 3
+		// "param1 \\\"same param\\\" param3" => 3
 	}
 
 }
