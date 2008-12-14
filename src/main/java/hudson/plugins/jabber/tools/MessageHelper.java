5d900c8f0f578a3800930f64923323d019c3b733
vsellier <vsellier@dev.java.net>
2008-12-14 18:50:48 +0000

    [jabber-plugin] take care of quote in commands

--- src/main/java/hudson/plugins/jabber/tools/MessageHelper.java
+++ src/main/java/hudson/plugins/jabber/tools/MessageHelper.java
@@ -2,8 +2,13 @@ package hudson.plugins.jabber.tools;
 
 import hudson.Util;
 import hudson.model.AbstractBuild;
-import hudson.model.Build;
 import hudson.model.Hudson;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
 
 /**
  * Utility class to help message creation
@@ -11,6 +16,12 @@ import hudson.model.Hudson;
  * @author vsellier
  */
 public class MessageHelper {
+	public final static Logger LOGGER = Logger.getLogger(MessageHelper.class
+			.toString());
+
+	private final static Pattern SPACE_PATTERN = Pattern.compile("\\s");
+	private final static String QUOTE = "\"";
+	
 	public static String getBuildURL(AbstractBuild<?, ?> lastBuild) {
 		// The hudson's base url
 		StringBuilder builder = new StringBuilder(Hudson.getInstance()
@@ -23,8 +34,43 @@ public class MessageHelper {
 		return builder.toString();
 
 	}
-	
+
 	public static String[] extractCommandLine(String message) {
-		return message.split("\\s");		
+		return extractParameters(message).toArray(new String[] {});
+	}
+
+	private static List<String> extractParameters(String commandLine) {
+		List<String> parameters = new ArrayList<String>();
+
+		// space protection
+		commandLine = commandLine.trim();
+		
+		if (commandLine.contains(QUOTE)) {
+			int firstQuote = commandLine.indexOf(QUOTE);
+			if (firstQuote == 0) {
+				// the first character is the quote
+				int end = commandLine.indexOf(QUOTE, 1);
+				parameters.add(commandLine.substring(1, end - 1));
+			} else {
+				// adding every thing before the first quote
+				parameters.addAll(extractParameters(commandLine.substring(0,
+						firstQuote)));
+				// adding the parameter between quotes
+				int endQuoted = commandLine.indexOf(QUOTE, firstQuote + 1);
+				parameters
+						.add(commandLine.substring(firstQuote + 1, endQuoted));
+
+				// adding everything after the quoted parameter into the
+				// parameters list
+				if (endQuoted < commandLine.length() - 1) {
+					parameters.addAll(extractParameters(commandLine
+							.substring(endQuoted + 1)));
+				}
+			}
+		} else {
+			// no quotes, just splitting on spaces
+			Collections.addAll(parameters, SPACE_PATTERN.split(commandLine));
+		}
+		return parameters;
 	}
 }
--- src/test/java/hudson/plugins/jabber/tools/MessageHelperTest.java
+++ src/test/java/hudson/plugins/jabber/tools/MessageHelperTest.java
@@ -12,18 +12,21 @@ public class MessageHelperTest {
 		assertEquals(1, MessageHelper.extractCommandLine("test  ").length);
 		assertEquals(3,
 				MessageHelper.extractCommandLine("test param1 param2").length);
-		// TODO commented to not break the global build
-		// assertEquals(2,
-		// MessageHelper.extractCommandLine("test \"same param\"").length);
-		// ' is not a separator
-		// assertEquals(3,
-		// MessageHelper.extractCommandLine("test 'same param'").length);
-		
-		// TODO add tests for :
-		// "param1 param's param3" => 3
-		// 'param1 "same param"' => 2
-		// 'param1 "same param" param3' => 3
-		// "param1 \\\"same param\\\" param3" => 3
-	}
 
+		assertEquals(3, MessageHelper.extractCommandLine("param1 \"same param\" param3").length);
+		assertEquals(2,
+				MessageHelper.extractCommandLine("test \"same param\"").length);
+		// ' is not a separator
+		assertEquals(2, MessageHelper
+				.extractCommandLine("param1 \"test 'same param'\"").length);
+
+		// several quoted arguments
+		assertEquals(3, MessageHelper
+				.extractCommandLine("param1 \"second param\" \"third param\"").length);
+
+		assertEquals(3, MessageHelper
+				.extractCommandLine("param1 param's param3").length);
+
+		assertEquals(1, MessageHelper.extractCommandLine("\"param1 param2\"").length);
+	}
 }
