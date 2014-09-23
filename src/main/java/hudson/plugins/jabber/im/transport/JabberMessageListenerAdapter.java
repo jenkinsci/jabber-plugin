a5b23bedbfa52d449eb9bfd81a4daaae8908353f
Florian Schmaus <flo@geekplace.eu>
2014-09-23 10:09:25 +0200

    Same as in ea56fc5 just for the non-MUC message listener

--- src/main/java/hudson/plugins/jabber/im/transport/JabberMessageListenerAdapter.java
+++ src/main/java/hudson/plugins/jabber/im/transport/JabberMessageListenerAdapter.java
@@ -7,8 +7,6 @@ import org.jivesoftware.smack.Chat;
 import org.jivesoftware.smack.MessageListener;
 import org.jivesoftware.smack.PacketListener;
 import org.jivesoftware.smack.packet.Message;
-import org.jivesoftware.smack.packet.PacketExtension;
-import org.jivesoftware.smackx.delay.packet.DelayInformation;
 
 /**
  * Wraps an {@link IMMessageListener} in a Smack {@link PacketListener}.
@@ -28,11 +26,9 @@ class JabberMessageListenerAdapter implements MessageListener {
     
 	@Override
 	public void processMessage(Chat chat, Message msg) {
-		// don't react to old messages
-        for (PacketExtension pe : msg.getExtensions()) {
-            if (pe instanceof DelayInformation) {
-                return; // simply bail out here, it's an old message
-            }
+        // don't react to old messages
+        if (msg.getExtension("x", "jabber:x:delay") != null) {
+            return; // simply bail out here, it's an old message
         }
 
         IMMessage imMessage = new IMMessage(msg.getFrom(),
